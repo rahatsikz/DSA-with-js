6929a9534af8ae3b2baf2d1629c20a5246314ea0
Md. Rahat Newaz Sikder <mailtorahat@proton.me>
2024-03-14 12:48:43 +0600

    [done] ~ time our code

---
+++ Big-O-Notation/add1.js
@@ -0,0 +1,14 @@
+function AddUpto(n) {
+    let total = 0;
+    for (let i = 0; i <= n; i++) {
+        total += i;
+    }
+    return total;
+}
+
+// console.log(AddUpto(4));
+
+const t1 = performance.now();
+AddUpto(1000000000)
+const t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
---
+++ Big-O-Notation/add2.js
@@ -0,0 +1,9 @@
+function AddUpto(n) {
+    return n * (n + 1) / 2;
+}
+
+// console.log(AddUpto(4));
+const t1 = performance.now();
+AddUpto(1000000000)
+const t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
